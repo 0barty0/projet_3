11dfddbd06497371aaab34f6cbb4e6fca14fb4cd
0fabien0 <0fabien0@gmail.com>
2018-04-04 13:03:53 +0200

    Add canvas with mouse marker

--- js/stations.js
+++ js/stations.js
@@ -111,6 +111,11 @@ window.onload = function () {
 
     // Booking button
     $('#booking_btn').click(function () {
+        $('#canvas_container').slideDown(400);
+    });
+
+    // Confirm button
+    $('#confirm_btn').click(function () {
         var booking = new Booking(station.number, station.name);
         sessionStorage.name = booking.name;
         sessionStorage.time = booking.time;
@@ -118,5 +123,62 @@ window.onload = function () {
         bookingStatus();
         clearInterval(intervalID);
         intervalID = setInterval("updateStatus();", 1000);
+    });
+
+    // Canvas confirmation
+    context = document.getElementById('canvas').getContext("2d");
+
+    var clickX = new Array();
+    var clickY = new Array();
+    var clickDrag = new Array();
+    var paint;
+
+    function addClick(x, y, dragging) {
+        clickX.push(x);
+        clickY.push(y);
+        clickDrag.push(dragging);
+    }
+
+    function redraw() {
+        context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+        context.stokeStyle = "#df4b26";
+        context.lineJoin = "round";
+        context.lineWidth = 5;
+
+        for (var i = 0; i < clickX.length; i++) {
+            context.beginPath();
+
+            if (clickDrag[i] && i) {
+                context.moveTo(clickX[i - 1], clickY[i - 1]);
+            } else {
+                context.moveTo(clickX[i] - 1, clickY[i] - 1);
+            }
+            context.lineTo(clickX[i], clickY[i]);
+            context.closePath();
+            context.stroke();
+        }
+    }
+
+    $('#canvas').mousedown(function (e) {
+        var mouseX = e.pageX - this.offsetLeft;
+        var mouseY = e.pageY - this.offsetTop;
+        paint = true;
+        addClick(mouseX, mouseY);
+        redraw();
+    });
+
+    $('#canvas').mousemove(function (e) {
+        if (paint) {
+            addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+            redraw();
+        }
+    });
+
+    $('#canvas').mouseup(function (e) {
+        paint = false;
+    });
+
+    $('#canvas').mouseleave(function (e) {
+        paint = false;
     });
 };
